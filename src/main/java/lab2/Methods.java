9661f34f99d7e84e99c3bf0a675f9824f6de6c04
nikitanosov1 <nikinosov2002@mail.ru>
2023-03-14 16:17:39 +0400

    lab2 done

--- src/main/java/lab2/Methods.java
+++ src/main/java/lab2/Methods.java
@@ -1,10 +1,18 @@
 package lab2;
 
+import lab1.Functions;
+
 import java.util.Arrays;
+import java.util.function.Function;
 
 public class Methods {
     public static double[] findMinimumInTheDirectionOfTheVector(MyFunction function, double[] startPoint, double[] direction) {
-        double[] step = new double[]{ direction[0] / direction[1], 1, direction[1] / direction[2]};
+        double lenOfDirection = calcNormOfVector(direction);
+        double[] step = new double[]{
+                direction[0] / lenOfDirection,
+                direction[1] / lenOfDirection,
+                direction[2] / lenOfDirection,
+        };
         double[] nextPoint = new double[] {
                 startPoint[0] + step[0],
                 startPoint[1] + step[1],
@@ -47,7 +55,39 @@ public class Methods {
             step[2] *= 2;
         }
 
+        //System.out.println("aPoint = " + Arrays.toString(startPoint));
+        //System.out.println("bPoint = " + Arrays.toString(nextPoint));
 
-        return nextPoint;
+        double[] vectorFromAtoB = new double[] {
+                nextPoint[0] - startPoint[0],
+                nextPoint[1] - startPoint[1],
+                nextPoint[2] - startPoint[2]
+        };
+        double lengthVectorFromAToB = calcNormOfVector(vectorFromAtoB);
+        //System.out.println("lengthVectorFromAToB " + lengthVectorFromAToB);
+
+        double[] finalNextPoint = nextPoint;
+        Function<Double, Double> hintFunction = (len) -> {
+            double x = (finalNextPoint[0] - startPoint[0]) / (lengthVectorFromAToB / len) + startPoint[0];
+            double y = (finalNextPoint[1] - startPoint[1]) / (lengthVectorFromAToB / len) + startPoint[1];
+            double z = (finalNextPoint[2] - startPoint[2]) / (lengthVectorFromAToB / len) + startPoint[2];
+
+            return function.apply(x, y, z);
+        };
+        double eps = Math.pow(10, -5);
+        double delta = Math.pow(10, -4);
+        double lenWhichGiveUsMinValueOfFunction = Functions.dichotomie(0.1, lengthVectorFromAToB, eps, delta, hintFunction).getMinX();
+
+        double x = (finalNextPoint[0] - startPoint[0]) / (lengthVectorFromAToB / lenWhichGiveUsMinValueOfFunction) + startPoint[0];
+        double y = (finalNextPoint[1] - startPoint[1]) / (lengthVectorFromAToB / lenWhichGiveUsMinValueOfFunction) + startPoint[1];
+        double z = (finalNextPoint[2] - startPoint[2]) / (lengthVectorFromAToB / lenWhichGiveUsMinValueOfFunction) + startPoint[2];
+
+        return new double[] {
+                x, y, z
+        };
+    }
+
+    private static double calcNormOfVector(double[] vector) {
+        return Math.sqrt(Math.pow(vector[0], 2) + Math.pow(vector[1], 2) + Math.pow(vector[2], 2));
     }
 }
--- src/main/java/lab2/Solution.java
+++ src/main/java/lab2/Solution.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class Solution {
     public static void solve() {
         MyFunction function = new MyFunction();
-        double[] startPoint = new double[]{3, 1, 4};
+        double[] startPoint = new double[]{4, 1, -1};
         double eps = Math.pow(10, -3);
 
-        double[] result = Methods.findMinimumInTheDirectionOfTheVector(function, startPoint, new double[]{1, 1, 0});
+        double[] result = Methods.findMinimumInTheDirectionOfTheVector(function, startPoint, new double[]{1, 1, 1});
 
         System.out.println(Arrays.toString(result));
     }
